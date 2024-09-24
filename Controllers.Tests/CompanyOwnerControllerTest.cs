16927860c247ae52de17d257b921892aa476d1ca
Agustin Madrid <aguzaram33@gmail.com>
2024-09-24 18:11:12 -0300

    [RED] Create Company Owner Test

---
+++ Controllers.Tests/CompanyOwnerControllerTest.cs
@@ -0,0 +1,30 @@
+using System.Diagnostics.CodeAnalysis;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Controllers.Tests;
+
+[ExcludeFromCodeCoverage]
+[TestClass]
+public class CompanyOwnerControllerTest
+{
+        private CompanyOwnerController _controller;
+    
+        [TestMethod]
+        public void CreateCompanyOwner_WhenAllPropertiesOk()
+        {
+            User user = new User("John", "Doe", "mail@mail.com", "123456@asd");
+            
+            Mock<IUserLogic> companyOwnerLogic = new Mock<IUserLogic>(MockBehavior.Strict);
+            companyOwnerLogic.Setup(x => x.CreateCompanyOwner(It.IsAny<User>()))
+                .Returns(user);
+            
+            _controller = new CompanyOwnerController(companyOwnerLogic.Object);
+            
+            var act = _controller.CreateCompanyOwner(user);
+            var expected = new OkObjectResult(user);
+            
+            act.Should().BeEquivalentTo(expected);
+        }
+}
