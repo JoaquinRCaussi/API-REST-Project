71d96a69cd315012d4953dcfa040ae64a27a84df
Agustin Madrid <aguzaram33@gmail.com>
2024-09-24 22:15:44 -0300

    [REFACTOR] Fixing usings

--- Controllers.Tests/CompaniesController.cs
+++ Controllers.Tests/CompaniesController.cs
@@ -1,13 +1,11 @@
 using System.Diagnostics.CodeAnalysis;
+using Domain;
+using LogicInterface;
+using WebApi.Controllers;
+using WebApi.Models;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
-
-using Domain;
-using LogicInterface;
-using WebApi.Models;
-using WebApi.Controllers;
-
 
 namespace Controllers.Tests;
 
