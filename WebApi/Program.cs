f9cd731426500dfc633635cc9837ba7d2a82780a
Joaquin Rodriguez <joaquinrodriguezcaussi@gmail.com>
2024-09-26 20:30:11 -0300

    Fixing format problems

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,1 +1,20 @@
-using System.Diagnostics.CodeAnalysis;WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);// Add services to the container.builder.Services.AddControllers();WebApplication? app = builder.Build();// Configure the HTTP request pipeline.app.UseHttpsRedirection();app.UseAuthorization();app.MapControllers();app.Run();[ExcludeFromCodeCoverage]public partial class Program{}
+using System.Diagnostics.CodeAnalysis;
+
+WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddControllers();
+WebApplication? app = builder.Build();
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+[ExcludeFromCodeCoverage]
+public partial class Program
+{
+}
