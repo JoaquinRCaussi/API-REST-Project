b6910acd905dce818c75f94270388f4c0a4b9753
Agustin Madrid <aguzaram33@gmail.com>
2024-09-26 01:09:44 -0300

    [REFACTOR] Fixing to acomplish code analysis

--- Domain/Role.cs
+++ Domain/Role.cs
@@ -4,8 +4,9 @@ public sealed record class Role
 {
     public Guid Id { get; set; }
     public string Name { get; init; } = string.Empty;
-    public List<PermissionKey> Permissions { get; } = new();
-
-    public bool HasPermission(PermissionKey permission) => Permissions.Contains(permission);
+    public List<PermissionKey> Permissions { get; set; } = [];
+    
+    public bool HasPermission(PermissionKey permission) => 
+        Permissions.Contains(permission);
+    
 }
-
