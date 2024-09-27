d50cd0d9752362048f310a2515bcb1bd8c3b3f2a
Agustin Madrid <aguzaram33@gmail.com>
2024-09-27 01:53:24 -0300

    [GREEN] HomeLogic GetHomes

---
+++ BusinessLogic.Test/HomeLogicTest.cs
@@ -0,0 +1,46 @@
+using System.Diagnostics.CodeAnalysis;
+using Domain;
+using FluentAssertions;
+using IDataAccess;
+using LogicInterface;
+using Moq;
+
+namespace BusinessLogic.Test;
+
+[ExcludeFromCodeCoverage]
+[TestClass]
+public class HomeLogicTest
+{
+    private Mock<IHomeRepository>? _homeRepositoryMock;
+    private IHomeLogic? _homeLogic;
+    
+    [TestInitialize]
+    public void Initialize()
+    {
+        _homeRepositoryMock = new Mock<IHomeRepository>();
+        _homeLogic = new HomeLogic(_homeRepositoryMock.Object);
+    }
+    
+    [TestMethod]
+    public void GetHomesTest()
+    {
+        var homes = new List<Home>
+        {
+            new Home
+            {
+                Id = Guid.NewGuid(),
+                Location = "Home",
+                HomeOwner = Guid.NewGuid(),
+                Members = new List<Guid> { Guid.NewGuid() },
+                MemberCount = 5,
+                Devices = "asd"
+            }
+        };
+        
+        _homeRepositoryMock.Setup(x => x.GetHomes()).Returns(homes);
+        
+        var result = _homeLogic.GetHomes();
+        
+        result.Should().BeEquivalentTo(homes);
+    }
+}
