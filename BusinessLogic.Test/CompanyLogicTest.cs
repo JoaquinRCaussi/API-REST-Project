7010c662fd7350de5b0e586b057faaf10d60b978
Joaquin Rodriguez <joaquinrodriguezcaussi@gmail.com>
2024-10-02 18:23:54 -0300

    [RED] CreateCompany_WhenTheOwnerHasAlreadyACompany

--- BusinessLogic.Test/CompanyLogicTest.cs
+++ BusinessLogic.Test/CompanyLogicTest.cs
@@ -42,5 +42,33 @@ public class CompanyLogicTest
 
         result.Should().BeEquivalentTo(company);
     }
-    
+
+
+    [TestMethod]
+    public void CreateCompany_WhenTheOwnerHasAlreadyACompany()
+    {
+        // Arrange
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            Name = "John",
+            LastName = "Snow",
+            Email = "mail@mail.com",
+            Password = "password@123",
+        };
+
+        var company = new Company { Id = Guid.NewGuid(), Name = "Company", RUT = "Address", Owner = user };
+        
+        user.Company = company;
+        user.CompanyID = company.Id;
+        
+        var mock = new Mock<ICompanyRepository>(MockBehavior.Strict);
+        mock.Setup(x => x.CreateCompany(company)).Returns(company);
+        var companyLogic = new CompanyLogic(mock.Object);
+        
+        // Act
+        Action act = () => companyLogic.CreateCompany(company);
+
+        act.Should().Throw<ConflictException>();
+    }
 }
