b93798a5646aec68c0173da948c8d9821c111974
Joaquin Rodriguez <joaquinrodriguezcaussi@gmail.com>
2024-10-06 11:56:55 -0300

    [REFACTOR] Code Analysis

--- DataAccess/Data/HMDbContext.cs
+++ DataAccess/Data/HMDbContext.cs
@@ -32,7 +32,7 @@ public class HMDbContext : DbContext
             .HasOne(d => d.Company)
             .WithMany()
             .HasForeignKey(d => d.CompanyId);
-            
+
 
         var userId = Guid.Parse("205e7ec9-673c-4db2-911d-10fe2b9c159a");
         var companyId = Guid.Parse("10570280-239e-4fb8-8939-4f37415fccb7");
--- DataAccess/Migrations/20241006145005_DeviceHasRepositoryAndTPH.cs
+++ DataAccess/Migrations/20241006145005_DeviceHasRepositoryAndTPH.cs
@@ -2,57 +2,56 @@
 
 #nullable disable
 
-namespace DataAccess.Migrations
+namespace DataAccess.Migrations;
+
+/// <inheritdoc />
+public partial class DeviceHasRepositoryAndTPH : Migration
 {
     /// <inheritdoc />
-    public partial class DeviceHasRepositoryAndTPH : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<bool>(
-                name: "Indoors",
-                table: "Devices",
-                type: "bit",
-                nullable: true);
+        migrationBuilder.AddColumn<bool>(
+            name: "Indoors",
+            table: "Devices",
+            type: "bit",
+            nullable: true);
 
-            migrationBuilder.AddColumn<bool>(
-                name: "Outdoors",
-                table: "Devices",
-                type: "bit",
-                nullable: true);
+        migrationBuilder.AddColumn<bool>(
+            name: "Outdoors",
+            table: "Devices",
+            type: "bit",
+            nullable: true);
 
-            migrationBuilder.AddColumn<bool>(
-                name: "SupportMovementDetection",
-                table: "Devices",
-                type: "bit",
-                nullable: true);
+        migrationBuilder.AddColumn<bool>(
+            name: "SupportMovementDetection",
+            table: "Devices",
+            type: "bit",
+            nullable: true);
 
-            migrationBuilder.AddColumn<bool>(
-                name: "SupportPersonDetection",
-                table: "Devices",
-                type: "bit",
-                nullable: true);
-        }
+        migrationBuilder.AddColumn<bool>(
+            name: "SupportPersonDetection",
+            table: "Devices",
+            type: "bit",
+            nullable: true);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "Indoors",
-                table: "Devices");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "Indoors",
+            table: "Devices");
 
-            migrationBuilder.DropColumn(
-                name: "Outdoors",
-                table: "Devices");
+        migrationBuilder.DropColumn(
+            name: "Outdoors",
+            table: "Devices");
 
-            migrationBuilder.DropColumn(
-                name: "SupportMovementDetection",
-                table: "Devices");
+        migrationBuilder.DropColumn(
+            name: "SupportMovementDetection",
+            table: "Devices");
 
-            migrationBuilder.DropColumn(
-                name: "SupportPersonDetection",
-                table: "Devices");
-        }
+        migrationBuilder.DropColumn(
+            name: "SupportPersonDetection",
+            table: "Devices");
     }
 }
