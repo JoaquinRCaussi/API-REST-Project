fd640f430a94cb49db51d8d21b25eb92bcea6973
Agustin Madrid <aguzaram33@gmail.com>
2024-10-23 01:44:22 -0300

    [GREEN] GetHomeDevices should return 204

--- Controllers.Tests/HomeControllerTest.cs
+++ Controllers.Tests/HomeControllerTest.cs
@@ -820,4 +820,40 @@ public class HomeControllerTest
 
         homeLogic.VerifyAll();
     }
+    
+    [TestMethod]
+    public void GetHomeDevices_ShouldReturnNoContent()
+    {
+        var homeId = Guid.NewGuid();
+        var homeLogic = new Mock<IHomeLogic>(MockBehavior.Strict);
+        homeLogic.Setup(x => x.GetHomeDevices(homeId))
+            .Throws(new EmptyException("No devices found for this home."));
+
+        var controller = new HomeController(homeLogic.Object, null);
+
+        Action act = () => controller.GetHomeDevices(homeId);
+
+        act.Should().Throw<EmptyException>();
+
+        var context = new ActionContext
+        {
+            HttpContext = new DefaultHttpContext(),
+            RouteData = new Microsoft.AspNetCore.Routing.RouteData(),
+            ActionDescriptor = new Microsoft.AspNetCore.Mvc.Controllers.ControllerActionDescriptor()
+        };
+
+        var exceptionFilter = new ExceptionFilter();
+        var exceptionContext = new ExceptionContext(context, new List<IFilterMetadata>())
+        {
+            Exception = new EmptyException("No devices found for this home.")
+        };
+
+        exceptionFilter.OnException(exceptionContext);
+
+        var result = exceptionContext.Result as ObjectResult;
+        result.Should().NotBeNull();
+        result.StatusCode.Should().Be((int)HttpStatusCode.NoContent);
+
+        homeLogic.VerifyAll();
+    }
 }
