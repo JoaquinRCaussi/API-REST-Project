98c0fcc956bb7b51ac76dc77b74f577bade229fd
Agustin Madrid <aguzaram33@gmail.com>
2024-10-23 01:19:49 -0300

    [GREEN] GetUser Should Throw Exception When No User Found

--- BusinessLogic.Test/UserLogicTest.cs
+++ BusinessLogic.Test/UserLogicTest.cs
@@ -142,19 +142,17 @@ public class UserLogicTest
             Email = "mail@mail.com",
             Password = "password@123"
         };
-
+        
+        _userRepositoryMock.Setup(x => x.ExistUser(user.Id)).Returns(true);
+        
         _userRepositoryMock.Setup(x => x.GetUser(user.Id)).Returns(user);
-
+        
         var result = _userLogic.GetUser(user.Id);
-
+        
         result.Should().BeEquivalentTo(user);
-
-        result.Id.Should().Be(user.Id);
-        result.Name.Should().Be(user.Name);
-        result.LastName.Should().Be(user.LastName);
-        result.Email.Should().Be(user.Email);
-        result.Password.Should().Be(user.Password);
-        result.Role.Should().Be(user.Role);
+        
+        _userRepositoryMock.Verify(x => x.GetUser(user.Id), Times.Once);
+        _userRepositoryMock.Verify(x => x.ExistUser(user.Id), Times.Once);
     }
 
     [TestMethod]
