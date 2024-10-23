6f33339fdf5eed66021c180759c01b66cccd8053
Agustin Madrid <aguzaram33@gmail.com>
2024-10-23 01:39:50 -0300

    [GREEN] GetHomes Should return No content

--- Controllers.Tests/HomeControllerTest.cs
+++ Controllers.Tests/HomeControllerTest.cs
@@ -1,12 +1,16 @@
 using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using BusinessLogic;
 using Domain;
 using FluentAssertions;
 using IBusinessLogic;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Models;
 using Moq;
 using WebApi.Controllers;
+using WebApi.Filters;
 
 namespace Controllers.Tests;
 
@@ -714,15 +718,34 @@ public class HomeControllerTest
     public void GetHomes_ShouldReturnNoContent()
     {
         var homeLogic = new Mock<IHomeLogic>(MockBehavior.Strict);
-        homeLogic.Setup(x => x.GetHomes()).Returns(new List<Home>());
+        homeLogic.Setup(x => x.GetHomes())
+            .Throws(new EmptyException("No homes found"));
 
         var controller = new HomeController(homeLogic.Object, null);
-        
-        IActionResult act = controller.GetHomes();
 
-        var noContentResult = act as NoContentResult;
-        Assert.IsNotNull(noContentResult, "Expected NoContentResult");
+        Action act = () => controller.GetHomes();
 
-        homeLogic.Verify(x => x.GetHomes(), Times.Once);
+        act.Should().Throw<EmptyException>();
+
+        var context = new ActionContext
+        {
+            HttpContext = new DefaultHttpContext(),
+            RouteData = new Microsoft.AspNetCore.Routing.RouteData(),
+            ActionDescriptor = new Microsoft.AspNetCore.Mvc.Controllers.ControllerActionDescriptor()
+        };
+
+        var exceptionFilter = new ExceptionFilter();
+        var exceptionContext = new ExceptionContext(context, new List<IFilterMetadata>())
+        {
+            Exception = new EmptyException("No homes found")
+        };
+
+        exceptionFilter.OnException(exceptionContext);
+
+        var result = exceptionContext.Result as ObjectResult;
+        result.Should().NotBeNull();
+        result.StatusCode.Should().Be((int)HttpStatusCode.NoContent);
+
+        homeLogic.VerifyAll();
     }
 }
