d92ff0874c36bcf07b8c3e95f316e86f9eb80ecf
JuanMen23 <jm224304@fi365.ort.edu.uy>
2024-11-07 19:13:25 -0300

    [GREEN]CreateNotificationSensorMovementDetected_WhenAllPropertiesOk

--- WebApi/Controllers/HomeController.cs
+++ WebApi/Controllers/HomeController.cs
@@ -178,6 +178,19 @@ public class HomeController : ControllerBase
     }
 
     [HttpPost]
+    [Route("{homeId}/movementSensor/{hardwareId}/movement-detected")]
+    [AuthorizationFilter]
+    public IActionResult CreateNotificationMovementDetectedSensor(Guid homeId, Guid hardwareId)
+    {
+        var sensorRequest = new SensorRequest();
+        var sensorEvent = "movement-detected";
+        sensorRequest.Event = sensorEvent;
+        var notifications = _homeLogic.CreateNotificationSensor(homeId, hardwareId, sensorRequest);
+        var notification = notifications.First();
+        return CreatedAtAction(nameof(CreateNotificationMovementDetectedSensor), new { id = notification.Id }, notifications);
+    }
+
+    [HttpPost]
     [Route("{homeId}/camera/{hardwareId}/person-detected")]
     [AuthorizationFilter]
     public IActionResult CreateNotificationPersonDetectedCamera(Guid homeId, Guid hardwareId)
