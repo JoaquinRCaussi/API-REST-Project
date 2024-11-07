e084884b56c41134327ea01905f45b933559f597
JuanMen23 <jm224304@fi365.ort.edu.uy>
2024-11-07 18:20:25 -0300

    [RED] CreateNotificationOpenMovementSensor_WhenAllPropertiesOk

--- Controllers.Tests/HomeControllerTest.cs
+++ Controllers.Tests/HomeControllerTest.cs
@@ -471,6 +471,53 @@ public class HomeControllerTest
     }
 
     [TestMethod]
+    public void CreateNotificationOpenMovementSensor_WhenAllPropertiesOk()
+    {
+        // Arrange
+        var homeId = Guid.NewGuid();
+        var hardwareId = Guid.NewGuid();
+        var sensorRequest = new SensorRequest
+        {
+            Event = "open"
+        };
+
+        var notification = new Notification
+        {
+            Id = Guid.NewGuid(),
+            Event = "Movement Sensor opened",
+            CreatedAt = DateTime.UtcNow,
+            IsRead = false,
+            HardwareId = hardwareId,
+            UserId = Guid.NewGuid()
+
+        };
+
+        var notifications = new List<Notification> { notification };
+
+        var homeLogic = new Mock<IHomeLogic>(MockBehavior.Strict);
+        homeLogic.Setup(x => x.CreateNotificationSensor(homeId, hardwareId, It.IsAny<SensorRequest>()))
+            .Returns(notifications);
+
+        var memberSettingLogic = new Mock<IMemberSettingLogic>(MockBehavior.Strict);
+
+        var controller = new HomeController(homeLogic.Object, memberSettingLogic.Object);
+
+        IActionResult act = controller.CreateNotificationOpenMovementSensor(homeId, hardwareId);
+
+        var expected = new CreatedAtActionResult(
+            nameof(controller.CreateNotificationOpenMovementSensor),
+            nameof(HomeController).Replace("Controller", ""),
+            new { id = notification.Id },
+            notifications
+        );
+
+        act.Should().BeEquivalentTo(expected, options => options
+            .ExcludingMissingMembers()
+            .Excluding(x => x.ControllerName)
+            .Excluding(x => x.RouteValues));
+    }
+
+    [TestMethod]
     public void CreateNotificationOpenSensor_WhenAllPropertiesOk()
     {
         // Arrange
