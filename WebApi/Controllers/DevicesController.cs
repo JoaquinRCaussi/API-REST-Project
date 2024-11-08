4fcf730e69f17c825b7db4332b541ed4811d2eb1
JuanMen23 <jm224304@fi365.ort.edu.uy>
2024-11-07 23:27:41 -0300

    [GREEN]CreateMovementSensor_WhenAllPropertiesOK_ShouldReturnCreated

--- WebApi/Controllers/DevicesController.cs
+++ WebApi/Controllers/DevicesController.cs
@@ -20,6 +20,18 @@ public class DevicesController : ControllerBase
     }
 
     [HttpPost]
+    [Route("movementSensor")]
+    [AuthorizationFilter("CanCreateADevice")]
+    public IActionResult CreateMovementSensor([FromBody] MovementSensorRequest device)
+    {
+        var user = HttpContext.Items[0] as User;
+        Device deviceToCreate = device.ToArgs(user.Company);
+        Device createdDevice = _deviceLogic.CreateDevice(deviceToCreate);
+        var response = new DeviceResponse(createdDevice);
+        return CreatedAtAction(nameof(CreateMovementSensor), new { id = createdDevice.Id }, response);
+    }
+
+    [HttpPost]
     [Route("windowSensor")]
     [AuthorizationFilter("CanCreateADevice")]
     public IActionResult CreateDevice([FromBody] DeviceRequest device)
