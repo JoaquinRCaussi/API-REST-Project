8e42fc60af70954b398ba99b09129a66f3cc24ff
JuanMen23 <jm224304@fi365.ort.edu.uy>
2024-11-07 23:19:13 -0300

    [GREEN]CreateWindowSensor_WhenAllPropertiesOK_ShouldReturnCreated

--- Controllers.Tests/DeviceControllerTest.cs
+++ Controllers.Tests/DeviceControllerTest.cs
@@ -56,7 +56,7 @@ public class DeviceControllerTest
             Name = "Dispositivo genérico",
             Company = _company,
             Description = "Descripción genérica",
-            DeviceType = DeviceType.Camera,
+            DeviceType = DeviceType.WindowSensor,
             Model = "Model X",
             Photo = "photo1.jpg"
         };
@@ -77,6 +77,43 @@ public class DeviceControllerTest
             SupportMovementDetection = true,
             SupportPersonDetection = false
         };
+    }
+
+    [TestMethod]
+    public void CreateWindowSensor_WhenAllPropertiesOK_ShouldReturnCreated()
+    {
+        _user.Company = _company;
+        var httpContext = new DefaultHttpContext();
+        httpContext.Items[0] = _user;
+        _controller!.ControllerContext.HttpContext = httpContext;
+        var device = CreateValidDevice();
+        var deviceRequest = new DeviceRequest()
+        {
+            Name = "Sensor de ventana genérica",
+            Description = "Descripción genérica",
+            DeviceType = DeviceType.WindowSensor,
+            Model = "Model X",
+            Photo = "photo1.jpg",
+        };
+        var expectedResponse = new DeviceResponse(device);
+
+        _deviceLogicMock!
+            .Setup(logic => logic.CreateDevice(It.IsAny<Device>()))
+            .Returns(device);
+
+        IActionResult result = _controller!.CreateDevice(deviceRequest);
+
+        var expected = new CreatedAtActionResult(
+            nameof(_controller.CreateDevice),
+            nameof(DevicesController).Replace("Controller", ""),
+            new { id = device.Id },
+            expectedResponse
+        );
+
+        result.Should().BeEquivalentTo(expected, options => options
+            .ExcludingMissingMembers()
+            .Excluding(x => x.ControllerName)
+            .Excluding(x => x.RouteValues));
     }
 
     [TestMethod]
