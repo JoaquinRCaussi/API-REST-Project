22d9bc53d847603afbf9bf2f46267a10da7d2016
Agus-Madrid <agustin.madrid@quanam.com>
2024-11-10 18:13:29 -0300

    [GREEN] CreateCameraTest_WhenValidatorIsInvalid_ShouldThrowException

--- BusinessLogic.Test/DevicesLogicTest.cs
+++ BusinessLogic.Test/DevicesLogicTest.cs
@@ -310,19 +310,19 @@ public class DevicesLogicTest
         _companyRepository = new Mock<ICompanyRepository>(MockBehavior.Strict);
         _validatorService = new Mock<ValidatorService>(MockBehavior.Strict);
 
-        _companyRepository.Setup(x => x.ExistsCompany(_device.Company.Id)).Returns(true);
-        _deviceRepository.Setup(x => x.ExistsDevice(_device.Name, _device.Company.Id)).Returns(false);
-        _deviceRepository.Setup(x => x.CreateDevice(It.IsAny<Device>())).Returns(camera);
+        _companyRepository.Setup(x => x.ExistsCompany(camera.Company.Id)).Returns(true);
+        _deviceRepository.Setup(x => x.ExistsDevice(camera.Name, camera.Company.Id)).Returns(false);
+        _deviceRepository.Setup(x => x.CreateCamera(It.IsAny<Camera>())).Returns(camera);
 
         var mockValidator = new Mock<IModeloValidador>();
-        _validatorService.Setup(v => v.GetValidatorByName(_device.Company.ValidatorModelName))
+        _validatorService.Setup(v => v.GetValidatorByName(camera.Company.ValidatorModelName))
             .Returns(mockValidator.Object);
 
         mockValidator.Setup(v => v.EsValido(It.IsAny<Modelo>())).Returns(false);
 
         var deviceLogic = new DeviceLogic(_deviceRepository.Object, _companyRepository.Object, _validatorService.Object);
 
-        Action act = () => deviceLogic.CreateDevice(camera);
+        Action act = () => deviceLogic.CreateCamera(camera);
 
         act.Should().Throw<NotValidDataException>()
             .WithMessage("The model is not valid");
