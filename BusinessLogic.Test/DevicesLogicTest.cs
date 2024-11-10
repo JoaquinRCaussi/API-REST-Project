acb9aeda0986cb0f1f2470a571107bf85b37f932
Agus-Madrid <agustin.madrid@quanam.com>
2024-11-10 17:58:07 -0300

    [RED] CreateDeviceTest_WhenModelIsNull_ShouldThrowException

--- BusinessLogic.Test/DevicesLogicTest.cs
+++ BusinessLogic.Test/DevicesLogicTest.cs
@@ -183,7 +183,40 @@ public class DevicesLogicTest
         act.Should().Throw<NotValidDataException>()
             .WithMessage("The company does not have a validator");
     }
+    
+    [TestMethod]
+    public void CreateDeviceTest_WhenModelIsNull_ShouldThrowException()
+    {
+        _device = new Device
+        {
+            Id = Guid.NewGuid(),
+            Name = "Device",
+            Model = "Model",
+            DeviceType = DeviceType.Camera,
+            Description = "Description",
+            Photo = "Photo.png",
+            Company = _company
+        };
 
+        _deviceRepository = new Mock<IDeviceRepository>(MockBehavior.Strict);
+        _companyRepository = new Mock<ICompanyRepository>(MockBehavior.Strict);
+        _validatorService = new Mock<ValidatorService>(MockBehavior.Strict);
+
+        _companyRepository.Setup(x => x.ExistsCompany(_device.Company.Id)).Returns(true);
+        _deviceRepository.Setup(x => x.ExistsDevice(_device.Name, _device.Company.Id)).Returns(false);
+        _deviceRepository.Setup(x => x.CreateDevice(It.IsAny<Device>())).Returns(_device);
+
+        var mockValidator = new Mock<IModeloValidador>();
+        _validatorService.Setup(v => v.GetValidatorByName(_device.Company.ValidatorModelName))
+            .Returns(mockValidator.Object);
+
+        var deviceLogic = new DeviceLogic(_deviceRepository.Object, _companyRepository.Object, _validatorService.Object);
+
+        Action act = () => deviceLogic.CreateDevice(_device);
+
+        act.Should().Throw<NotValidDataException>()
+            .WithMessage("The model is required");
+    }
 
     [TestMethod]
     public void CreateCameraTest_WhenAllPropertiesAreOk()
