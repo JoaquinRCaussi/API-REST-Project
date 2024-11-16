a68f9c59eb0c62f414c4d7f608e6621dda501d08
JuanMen23 <jm224304@fi365.ort.edu.uy>
2024-11-16 12:51:52 -0300

    [REFACTOR]GetUsers_WhenAllPropertiesOk

--- Controllers.Tests/UserControllerTest.cs
+++ Controllers.Tests/UserControllerTest.cs
@@ -91,7 +91,7 @@ public class UserControllerTest
         }
     };
 
-        userLogicMock.Setup(logic => logic.GetUsersFiltered(null, null)).Returns(expectedUsers);
+        userLogicMock.Setup(logic => logic.GetUsersFiltered(null, null, 1, 10)).Returns((expectedUsers, expectedUsers.Count));
 
         var controller = new UserController(userLogicMock.Object, homeLogicMock.Object);
 
