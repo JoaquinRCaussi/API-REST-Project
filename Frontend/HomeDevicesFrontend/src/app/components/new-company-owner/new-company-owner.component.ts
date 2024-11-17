5a9688ae8dac93ce7f215c8a38267191d5116368
JuanMen23 <jm224304@fi365.ort.edu.uy>
2024-11-17 15:31:57 -0300

    new companyOwner component

---
+++ Frontend/HomeDevicesFrontend/src/app/components/new-company-owner/new-company-owner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-company-owner',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-company-owner.component.html',
+  styleUrl: './new-company-owner.component.css'
+})
+export class NewCompanyOwnerComponent {
+
+}
