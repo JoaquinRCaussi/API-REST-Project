7f18b4875e9b1886a5366f92c533a228b584ed9e
Agus-Madrid <agustin.madrid@quanam.com>
2024-11-17 10:24:56 -0300

    [REFACTOR] Request in Room and some fixes

---
+++ WebApi/Models/In/AddRoomRequest.cs
@@ -0,0 +1,6 @@
+namespace WebApi.Models.In;
+
+public class AddRoomRequest
+{
+    public required string RoomName { get; set; }
+}
---
+++ WebApi/Models/Out/NewRoomResponse.cs
@@ -0,0 +1,8 @@
+namespace WebApi.Models.Out;
+
+public class NewRoomResponse
+{
+    public Guid? Id { get; set; }
+    public string? Name { get; set; }
+    
+}
