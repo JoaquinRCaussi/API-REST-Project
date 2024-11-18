41d8dfdef7536036918261fe3a4b4f96d02f6221
Agus-Madrid <agustin.madrid@quanam.com>
2024-11-17 21:01:12 -0300

    [GREEN] ValidatorController LoadValidators_WhenAllOk

--- BusinessLogic/Validators/ValidatorService.cs
+++ BusinessLogic/Validators/ValidatorService.cs
@@ -19,7 +19,7 @@ public class ValidatorService
         }
     }
 
-    public List<string> ChargeValidators()
+    public virtual List<string> ChargeValidators()
     {
         // Cargar cada DLL que esté en la carpeta Validators
         foreach (var dllPath in Directory.GetFiles(_pluginsPath, "*.dll"))
