913b2edb462ee97993a704bb4083330243e71711
JuanMen23 <jm224304@fi365.ort.edu.uy>
2024-11-18 18:44:49 -0300

    [REFACTOR]GetCompaniesTest

--- DataAccess.Tests/CompaniesRepositoryTest.cs
+++ DataAccess.Tests/CompaniesRepositoryTest.cs
@@ -71,7 +71,7 @@ public class CompaniesRepositoryTest
         SeedData(context);
         var repository = new CompanyRepository(context);
 
-        var result = repository.GetCompanies("Company", "John");
+        var (result, totalResults) = repository.GetCompanies("Company", "John", 1, 10);
 
         result.Should().NotBeNull();
         result.Should().HaveCount(1);
